ca50bacb9440ad3adc1f3733ea9ee7840317f031
Julian <julianchandrasutadi@gmail.com>
2026-04-26 22:11:49 +0700

    add hive script to ensure compat with git bash

--- hive-init.sh
+++ hive-init.sh
@@ -1,1 +1,10 @@
-docker compose exec hive-server2 /opt/hive/bin/schematool -initSchema -dbType hive -metaDbType postgres -url jdbc:hive2://localhost:10000/default
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Prevent Git Bash (MSYS) from rewriting Linux container paths like /opt/... to C:/...
+MSYS_NO_PATHCONV=1 docker compose exec hive-server2 \
+	/opt/hive/bin/schematool \
+	-initSchema \
+	-dbType hive \
+	-metaDbType postgres \
+	-url jdbc:hive2://localhost:10000/default
